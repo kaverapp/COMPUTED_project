90d812557b0e70796e0cec2cb475690e71313a67
kaverapp <138514864+kaverapp@users.noreply.github.com>
2024-09-28 10:41:36 +0530

    added cloudinary and multer

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import {DB_NMAE} from "../constants.js";
 
+let DB_url=process.env.MONGODB_url || "mongodb://127.0.0.1:27017";
 
 const connectDB=async()=>{
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_url}/${DB_NMAE}`)
+        const connectionInstance=await mongoose.connect(`${DB_url}/${DB_NMAE}`)
         console.log(`\n mongodb connected !* DB HOST : ${connectionInstance.connection.host}`);
         
     } catch (error) {
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,55 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+    });
+    
+(async function(localFilePath) {
+    
+    if(!localFilePath) return null;
+    // Upload an image
+    try {
+        const uploadResult = await cloudinary.uploader
+       .upload(
+               localFilePath, {
+                resource_type: "auto",
+               
+           }
+       )
+       //file has been uploaded successfully
+       console.log("file has been uploaded successfully on cloudinary",uploadResult.url)
+       return uploadResult 
+    } catch (error) {
+        fs.unlinkSync(localFilePath)      //remove the locally saved temperoary file as the upload operaton get failed
+        console.log("some rror uploading to cloudinary!!!");
+        return null;
+        
+    }
+       
+     
+     
+    
+
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    // const optimizeUrl = cloudinary.url('shoes', {
+    //     fetch_format: 'auto',
+    //     quality: 'auto'
+    // });
+    
+    // console.log(optimizeUrl);
+    
+    // // Transform the image: auto-crop to square aspect_ratio
+    // const autoCropUrl = cloudinary.url('shoes', {
+    //     crop: 'auto',
+    //     gravity: 'auto',
+    //     width: 500,
+    //     height: 500,
+    // });
+    
+    // console.log(autoCropUrl);    
+})();
